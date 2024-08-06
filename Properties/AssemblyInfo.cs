07ce44fa8e63535eb84d18bf9c84d24af453668e
Santiago David García Ramírez <78332911+dxuciv@users.noreply.github.com>
2024-08-05 19:23:39 -0600

    Xopmizer

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xopmizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xopmizer")]
+[assembly: AssemblyCopyright("Copyright © David García 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f5d3a95-65b2-4f64-8c2b-7c70296b9a7b")]
+
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
